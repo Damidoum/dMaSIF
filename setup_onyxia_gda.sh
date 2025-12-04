a6462fcdc848e1f98704c5eb2085ec6e5f5a1a5c
Jules Royer <jlr18@free.fr>
2025-12-04 10:15:32 +0100

    updated onyxia setup

--- setup_onyxia_gda.sh
+++ setup_onyxia_gda.sh
@@ -3,10 +3,18 @@ bash miniconda.sh -b -p /home/onyxia/work/miniconda
 source /home/onyxia/work/miniconda/bin/activate
 conda init bash
 
+exit  # kills the terminal to operate changes
+
+# reopen a new terminal - you should see '(base)' on the left
+# of a bash terminal line
+
+cd /home/onyxia/work/
 git clone https://github.com/Damidoum/dMaSIF.git
-cd /home/onyxia/work/dMaSIF
+cd dMaSIF
 
 conda create -n dmasif
+# you will be prompt to accept 2 terms & condition (press "a" * 2)
+# you will be prompt to start the download (press "y")
 conda activate dmasif
 
 ./setup.sh
